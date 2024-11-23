765f8e6e8112d4596f6618aeccbf0e91bdd3a1f3
Archisha <mail2archishap@gmail.com>
2024-11-23 22:30:58 +0530

    Javascript intro activity completed

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("myheading").innerHTML = "Heading from Javascript";
+
+// Add content in Javascript
+document.getElementById("myparagraph").innerHTML = "We are learning about Javascript today";
+
+//Create variable
+name1 = "Javascript"; // String Data type
+num1 = 34; // Number Data type
+num2 = 10; // Number Data type
+b = false; // Boolean Data type
+
+// Operators: Arithmectic Operators
+addition = num1 + num2;
+document.write(addition)
+document.write("<br>")
+subtract = num1 - num2;
+document.write(subtract)
+modulus = num1 % num2;
+
+// Operators: Assignment Operators
+num3 = 6;
+a = 10;
+b = 5;
+
+//Addition Assignment
+a += b
+// a = a + b
+b += a
+// b = b +1
+
+//Subtraction Assignment 
+a -= b
+// a = a - b
+
+a *= b
+a /= b
+a %= b
+
